1412bed65708a7775e92ad49721b9123efce9d0b
Benedict Bobis <bobis.benedict@gmail.com>
2025-01-26 16:42:15 -0700

    combined Leaf and Parent node to HTMLNode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -22,3 +22,101 @@ class TestHTMLNode(unittest.TestCase):
         actual = node.props_to_html()
         expected = 'a="1" b="2"'
         self.assertEqual(actual, expected)
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_to_html_no_value(self):
+        node = LeafNode(None, "")
+        with self.assertRaises(ValueError):
+            node.to_html()
+
+    def test_to_html_no_tag(self):
+        node = LeafNode(None, "Hello World")
+        self.assertEqual(node.to_html(), "Hello World")
+
+    def test_to_html_no_props(self):
+        node = LeafNode("button", "Click me!")
+        self.assertEqual(node.to_html(), "<button>Click me!</button>")
+
+    def test_to_html(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(
+            node.to_html(), '<a href="https://www.google.com">Click me!</a>'
+        )
+
+
+class TestParentNode(unittest.TestCase):
+
+    def test_to_html_no_tag(self):
+        node = ParentNode(None, children=[])
+        with self.assertRaises(ValueError) as ctx:
+            node.to_html()
+        self.assertEqual(str(ctx.exception), "tag is required")
+
+    def test_to_html_no_children(self):
+        node = ParentNode("p", None)
+        with self.assertRaises(ValueError) as ctx:
+            node.to_html()
+        self.assertEqual(str(ctx.exception), "children is required")
+
+    def test_to_html_empty_children(self):
+        node = ParentNode("p", [])
+        with self.assertRaises(ValueError) as ctx:
+            node.to_html()
+        self.assertEqual(str(ctx.exception), "children is required")
+
+    def test_to_html_with_one_node(self):
+        node = ParentNode(
+            "ul",
+            [
+                LeafNode("li", "Learn Python"),
+            ],
+        )
+
+        expected = "<ul><li>Learn Python</li></ul>"
+        self.assertEqual(
+            node.to_html(),
+            expected,
+        )
+
+    def test_to_html_with_multiple_nodes(self):
+        node = ParentNode(
+            "ul",
+            [
+                LeafNode("li", "Learn Python"),
+                LeafNode("li", "Learn Go"),
+            ],
+        )
+
+        expected = "<ul><li>Learn Python</li><li>Learn Go</li></ul>"
+        self.assertEqual(
+            node.to_html(),
+            expected,
+        )
+
+    def test_to_html_with_nested_nodes(self):
+        node = ParentNode(
+            "div",
+            [
+                LeafNode("h1", "TODO:"),
+                LeafNode(None, "These are my todo tasks"),
+                ParentNode(
+                    "div",
+                    [
+                        ParentNode(
+                            "ul",
+                            [
+                                LeafNode("li", "Learn Python"),
+                                LeafNode("li", "Learn Go"),
+                            ],
+                        ),
+                    ],
+                    {"class": "bg-gray"},
+                ),
+            ],
+        )
+        expected = '<div><h1>TODO:</h1>These are my todo tasks<div class="bg-gray"><ul><li>Learn Python</li><li>Learn Go</li></ul></div></div>'
+        self.assertEqual(
+            node.to_html(),
+            expected,
+        )
