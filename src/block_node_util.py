b8dbbf002bce9c2085d6333964abcdae230f4e1d
Benedict Bobis <bobis.benedict@gmail.com>
2025-01-31 08:48:52 -0700

    block type helper

--- src/block_node_util.py
+++ src/block_node_util.py
@@ -1,5 +1,42 @@
+import re
+from enum import Enum
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+
 def markdown_to_block(markdown: str):
     blocks = []
     for block in markdown.split("\n\n"):
         blocks.append("\n".join(map(lambda s: s.strip(), block.strip().split("\n"))))
     return blocks
+
+
+def block_to_block_type(markdown_block_text: str):
+    total_lines = len(markdown_block_text.split("\n"))
+
+    if len(re.findall(r"^\#{1,6}\s.+", markdown_block_text, re.M)) == total_lines:
+        return BlockType.HEADING
+
+    if re.findall(r"^`{3}[\s\S]+`{3}$", markdown_block_text, re.M):
+        return BlockType.CODE
+
+    if len(re.findall(r"^>\s*.+$", markdown_block_text, re.M)) == total_lines:
+        return BlockType.QUOTE
+
+    if len(re.findall(r"^[*-]\s.+$", markdown_block_text, re.M)) == total_lines:
+        return BlockType.UNORDERED_LIST
+
+    if (
+        len(re.findall(r"^[123456789]+[\d]*\.\s.+$", markdown_block_text, re.M))
+        == total_lines
+    ):
+        return BlockType.ORDERED_LIST
+
+    return BlockType.PARAGRAPH
--- src/test_block_node_util.py
+++ src/test_block_node_util.py
@@ -1,6 +1,6 @@
 import unittest
 
-from block_node_util import markdown_to_block
+from block_node_util import BlockType, block_to_block_type, markdown_to_block
 
 
 class TestMarkdownToBlock(unittest.TestCase):
@@ -23,3 +23,57 @@ class TestMarkdownToBlock(unittest.TestCase):
                 "* This is the first list item in a list block\n* This is a list item\n* This is another list item",
             ],
         )
+
+
+class TestBlockToBlockType(unittest.TestCase):
+    def test_heading_levels(self):
+        text = "# heading"
+        self.assertEqual(block_to_block_type(text), BlockType.HEADING)
+        text = "## heading"
+        self.assertEqual(block_to_block_type(text), BlockType.HEADING)
+        text = "### heading"
+        self.assertEqual(block_to_block_type(text), BlockType.HEADING)
+        text = "#### heading"
+        self.assertEqual(block_to_block_type(text), BlockType.HEADING)
+        text = "##### heading"
+        self.assertEqual(block_to_block_type(text), BlockType.HEADING)
+        text = "###### heading"
+        self.assertEqual(block_to_block_type(text), BlockType.HEADING)
+        text = "####### heading"
+        self.assertEqual(block_to_block_type(text), BlockType.PARAGRAPH)
+        text = "#heading"
+        self.assertEqual(block_to_block_type(text), BlockType.PARAGRAPH)
+
+    def test_code_block(self):
+        text = "```code block```"
+        self.assertEqual(block_to_block_type(text), BlockType.CODE)
+        text = """```
+                code block
+        ```"""
+        self.assertEqual(block_to_block_type(text), BlockType.CODE)
+
+    def test_quote_block(self):
+        text = "> quotes 1\n>quote 2"
+        self.assertEqual(block_to_block_type(text), BlockType.QUOTE)
+        text = "+ quotes 1\n>quote 2"
+        self.assertEqual(block_to_block_type(text), BlockType.PARAGRAPH)
+
+    def test_unordered_list_block(self):
+        text = "* list 1\n* list"
+        self.assertEqual(block_to_block_type(text), BlockType.UNORDERED_LIST)
+        text = "- list 1\n- list"
+        self.assertEqual(block_to_block_type(text), BlockType.UNORDERED_LIST)
+        text = "- list 1\n* list"
+        self.assertEqual(block_to_block_type(text), BlockType.UNORDERED_LIST)
+        text = "-list 1\n*list"
+        self.assertEqual(block_to_block_type(text), BlockType.PARAGRAPH)
+
+    def test_ordered_list_block(self):
+        text = "1. list 1\n2. list"
+        self.assertEqual(block_to_block_type(text), BlockType.ORDERED_LIST)
+        text = "0. list 1"
+        self.assertEqual(block_to_block_type(text), BlockType.PARAGRAPH)
+        text = "1.list 1"
+        self.assertEqual(block_to_block_type(text), BlockType.PARAGRAPH)
+        text = "a. list 1"
+        self.assertEqual(block_to_block_type(text), BlockType.PARAGRAPH)
